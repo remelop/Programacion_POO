61cd4a149d098a22dfcaccedf74229ee5f044973
remelop <re.melop@uea.edu.ec>
2024-06-30 14:31:19 -0500

    El programa transforma las unidades de medida. Permite convertir entre kilogramos y libras y entre kilómetros y millas. Utiliza varios tipos de datos, como enteros, flotantes, cadenas y booleanos.

--- Tipos de datos, Identificadores, Implementación y Publicación.py
+++ Tipos de datos, Identificadores, Implementación y Publicación.py
@@ -2,18 +2,6 @@
 El programa transforma las unidades de medida. Permite convertir entre kilogramos y libras y entre kilómetros y millas. Utiliza varios tipos de datos, como enteros, flotantes, cadenas y booleanos.
 """
 
-# Función para convertir kilómetros a millas
-def km_a_millas(kilometros):
-    millas_por_kilometro = 0.62
-    millas = kilometros * millas_por_kilometro
-    return millas
-
-# Función para convertir millas a kilómetros
-def millas_a_km(millas):
-    kilometros_por_milla = 1.60
-    kilometros = millas * kilometros_por_milla
-    return kilometros
-
 # Función para convertir kilogramos a libras
 def kg_a_libras(kilogramos):
     libras_por_kg = 2.20
@@ -28,10 +16,8 @@ def libras_a_kg(libras):
 
 # Solicitar al usuario que elija el tipo de conversión
 print("Seleccione el tipo de conversión:")
-print("1. Kilómetros a Millas")
-print("2. Millas a Kilómetros")
-print("3. Kilogramos a Libras")
-print("4. Libras a Kilogramos")
+print("1. Kilogramos a Libras")
+print("2. Libras a Kilogramos")
 
 opcion = int(input("Ingrese el número de la opción deseada: "))
 
@@ -40,18 +26,10 @@ opcion_valida = True
 
 # Realizar la conversión según la opción elegida
 if opcion == 1:
-    kilometros = float(input("Ingrese la cantidad en kilómetros: "))
-    millas = km_a_millas(kilometros)
-    print(f"{kilometros} kilómetros son {millas} millas.")
-elif opcion == 2:
-    millas = float(input("Ingrese la cantidad en millas: "))
-    kilometros = millas_a_km(millas)
-    print(f"{millas} millas son {kilometros} kilómetros.")
-elif opcion == 3:
     kilogramos = float(input("Ingrese la cantidad en kilogramos: "))
     libras = kg_a_libras(kilogramos)
     print(f"{kilogramos} kilogramos son {libras} libras.")
-elif opcion == 4:
+elif opcion == 2:
     libras = float(input("Ingrese la cantidad en libras: "))
     kilogramos = libras_a_kg(libras)
     print(f"{libras} libras son {kilogramos} kilogramos.")
